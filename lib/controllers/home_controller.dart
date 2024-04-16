59e96b0c77ba6a3f179bbf9f4202c4a2e7197de4
Hasith manohara <manoharauh.20@uom.lk>
2024-04-16 22:32:55 +0530

    json-serializble-generate

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,15 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
 class HomeController extends GetxController {
   String test = "this is test string";
 
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  late CollectionReference productCollection;
+
   @override
   void onInit() {
-    // TODO: implement onInit
+    productCollection = firestore.collection('product');
     super.onInit();
   }
 
   testMethod() {
     print(test);
   }
+
+  //add product to the firestore
 }
